bf78f5a6e8fcef93955209f931d87d91bdfdbcd0
TaeHyeon Kim <kth990303@naver.com>
2021-06-21 16:07:07 +0900

    토큰 생성 및 로그인 구현

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app=express();
 const port=3000;
+const cookieParser=require('cookie-parser');
 const { User }=require("./models/User");
-
 const config=require('./config/key');
 
 app.use(express.urlencoded({
@@ -39,6 +39,39 @@ app.post('/register', (req, res)=>{
             success: true
         });
     });
+});
+
+app.post('/login', (req, res)=>{
+    // 요청된 이메일을 db에서 찾는다.
+    User.findOne({email: req.body.email}, (err, user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "Unvalid email"
+            });
+        }
+        // 요청된 이메일이 db에 있다면 비밀번호 일치여부 확인
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch)
+                return res.json({
+                    loginSuccess:false,
+                    message:"Wrong password"
+                });
+            // 일치 시, 토큰 생성
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                // 토큰을 쿠키에 저장
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true,
+                    userId: user._id
+                });
+            });
+            
+        });
+    });
+    
 });
 
 app.listen(port, ()=>{
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const bcrypt=require('bcrypt');
 const saltRounds=10;
+const jwt=require('jsonwebtoken');
 
 const userSchema=mongoose.Schema({
     name:{
@@ -51,6 +52,23 @@ userSchema.pre('save', function(next){
     }
     else next();
 });
+
+userSchema.methods.comparePassword=function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken=function(cb){
+    const user=this;
+    const token=jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token=token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    });
+}
 
 const User=mongoose.model('User', userSchema);
 
