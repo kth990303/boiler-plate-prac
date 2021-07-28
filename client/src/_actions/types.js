b613046754ca3a46d585ef724db43212a048aaf0
TaeHyeon Kim <kth990303@naver.com>
2021-07-28 21:37:19 +0900

    redux를 이용한 로그인 구현

---
+++ client/src/_actions/types.js
@@ -0,0 +1,3 @@
+
+
+export const LOGIN_USER="login_user";
--- client/src/_components/views/LandingPage/LandingPage.js
+++ client/src/_components/views/LandingPage/LandingPage.js
@@ -7,8 +7,11 @@ function LandingPage() {
     .then(res=>console.log(res.data));
   }, [])
   return (
-    <div>
-      LandingPage
+    <div style={{
+      display:'flex', justifyContent:'center', alignItems:'center',
+      width:'100%', 'height':'100vh', backgroundColor:'#777799'
+    }}>
+      <h2>시작 페이지</h2>
     </div>
   )
 }
